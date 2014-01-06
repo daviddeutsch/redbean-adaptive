514c0f150c61e382a8015b80eeb9523f74b8cfc3
David Deutsch <skore@valanx.org>
2014-01-06 19:24:23 +0100

    again reworking the API a little, adding proper listeners

--- RedBean/Pipeline.php
+++ RedBean/Pipeline.php
@@ -14,33 +14,81 @@ class RedBean_Pipeline
 		$this->r->prefix('sys_pipeline_');
 	}
 
-	public function addClient()
+	public function addExternalListener( $name )
 	{
 		$listener = $this->r->dispense('listener');
+
+		$listener->name      = $name;
+		$listener->location  = 'external';
+		$listener->created   = $this->r->isoDateTime();
+		$listener->last_call = $this->r->isoDateTime();
 
 		return $this->r->store($listener);
 	}
 
-	public function followResource( $listener, $resource )
+	public function getUpdatesForListener( $name )
 	{
-		$resource = $this->r->x->resource->path($resource)->find(true);
+		$listener = $this->r->x->one->listener->name($name);
 
-		$listener = $this->r->x->listener->sha($listener)->find(true);
+		$listener->last_call = $this->r->isoDateTime();
 
-		$this->r->associate($resource, $listener);
+		$updates = $this->r->x->all->update->related($listener)->find();
+
+		$output = array();
+		foreach ( $updates as $update ) {
+			$output[] = $update->export();
+
+			$this->r->unassociate($listener, $update);
+		}
+	}
+
+	public function subscribe( $listener, $resource )
+	{
+		$listener = $this->r->x->one->listener->name($listener)->find();
+
+		if ( empty($listener->id) ) return false;
+
+		$resource = $this->r->x->one->resource->path($resource)->find(true);
+
+		return $this->r->associate($resource, $listener);
+	}
+
+	public function emit( $update )
+	{
+		// TODO: Rework to support genuine path support w/ permutations
+		$listeners = array_unique(
+			array_merge(
+				$this->r->x->resource
+					->path($update->path)->find(true)->sharedListener,
+				$this->r->x->resource
+					->path($update->type)->find(true)->sharedListener
+			)
+		);
+
+		foreach( $listeners as $listener ) {
+			if ( $listener->location == 'external' ) {
+				$this->r->associate($listener, $update);
+			} else {
+				// TODO: Support internal callbacks
+			}
+		}
 	}
 
 	public function add( $bean )
 	{
-		$this->r->_(
-			'event',
-			array(
-				'operation' => 'add',
-				'path' => $bean->getMeta('type') . '/' . $bean->id,
-				'object' => json_encode($bean),
-				'created' => $this->r->isoDateTime()
-			),
-			true
+		$this->emit(
+			$this->r->_(
+				'update',
+				array(
+					'operation' => 'add',
+					'path' => $bean->getMeta('type') . '/' . $bean->id,
+					'type' => $bean->getMeta('type'),
+					'object_id' => $bean->id,
+					'object' => json_encode($bean),
+					'created' => $this->r->isoDateTime()
+				),
+				true
+			)
 		);
 	}
 
@@ -50,30 +98,38 @@ class RedBean_Pipeline
 
 		if ( empty($changes) ) return;
 
-		$this->r->_(
-			'event',
-			array(
-				'operation' => 'update',
-				'path' => $bean->getMeta('type') . '/' . $bean->id,
-				'object' => json_encode($bean),
-				'created' => $this->r->isoDateTime()
-			),
-			true
+		$this->emit(
+			$this->r->_(
+				'update',
+				array(
+					'operation' => 'update',
+					'path' => $bean->getMeta('type') . '/' . $bean->id,
+					'type' => $bean->getMeta('type'),
+					'object_id' => $bean->id,
+					'object' => json_encode($bean),
+					'created' => $this->r->isoDateTime()
+				),
+				true
+			)
 		);
 	}
 
 
 	public function delete( $bean )
 	{
-		$this->r->_(
-			'event',
-			array(
-				'operation' => 'remove',
-				'path' => $bean->getMeta('type') . '/' . $bean->id,
-				'object' => json_encode($bean),
-				'created' => $this->r->isoDateTime()
-			),
-			true
+		$this->emit(
+			$this->r->_(
+				'update',
+				array(
+					'operation' => 'remove',
+					'path' => $bean->getMeta('type') . '/' . $bean->id,
+					'type' => $bean->getMeta('type'),
+					'object_id' => $bean->id,
+					'object' => json_encode($bean),
+					'created' => $this->r->isoDateTime()
+				),
+				true
+			)
 		);
 	}
 }
